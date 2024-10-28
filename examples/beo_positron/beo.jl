d3248bd6376d72755fdfb87f4771585e59a08a40
Kristoffer Simula <k.simula@fkf.mpg.de>
2024-10-28 14:02:57 +0100

    Working examples

--- examples/beo_positron/beo.jl
+++ examples/beo_positron/beo.jl
@@ -6,7 +6,11 @@ geometry="bohr
      O  0.000000 0.000000 2.515"
 
 
-basis="def2-sv(p)"
+#basis="def2-sv(p)"
+basis = Dict("ao"=>"cc-pVDZ",
+             "jkfit"=>"def2-universal-jkfit",
+             "mp2fit"=>"cc-pvdz-rifit")
+@set scf guess=:HCORE
 
 @dfhf
 
--- examples/lih_positron/lih.jl
+++ examples/lih_positron/lih.jl
@@ -6,7 +6,12 @@ geometry="bohr
      H  0.000000 0.000000 3.0196"
 
 
-basis="def2-sv(p)"
+#basis="def2-sv(p)"
+#basis="vdz"
+basis = Dict("ao"=>"cc-pVDZ",
+             "jkfit"=>"def2-universal-jkfit",
+             "mp2fit"=>"cc-pvdz-rifit")
+@set scf guess=:HCORE
 
 @dfhf
 
