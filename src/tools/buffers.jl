255934b80dbd4bffe94d84fd914aa8b28179f58b
Daniel Kats <d.kats@fkf.mpg.de>
2024-11-13 08:24:49 +0100

    Add function to repair threaded buffers
    sometimes useful in REPL...

--- src/tools/buffers.jl
+++ src/tools/buffers.jl
@@ -21,7 +21,7 @@ the [`neuralyze`](@ref) function can be used to wipe the memory about the origin
 module Buffers
 
 export Buffer, ThreadsBuffer
-export alloc!, drop!, reset!
+export alloc!, drop!, reset!, repair!
 export reshape_buf!
 export used, nbuffers, with_buffer
 export neuralyze
@@ -190,18 +190,21 @@ end
 """
     ThreadsBuffer{T}
 
-Buffer object to store data of type `T` with an offset for each thread.
+Buffer object to store data of type `T` for each thread.
 
-See [`Buffer`](@ref) for more details.
+By default, the buffer is created for `nthreads()` threads,
+i.e., each thread has its own buffer [`Buffer`](@ref).
+
+Create the buffer with `ThreadsBuffer{T}(len, nbuf=Threads.nthreads())` and use it with [`alloc!`](@ref), [`drop!`](@ref), [`reset!`](@ref), etc.
 
 !!! warning "Warning"
     Always [`reset!`](@ref) or [`Buffers.release!`](@ref) the buffer after use!
 
 # Example
 ```julia
-julia> buf = Buffer(100000)
+julia> buf = Buffer(10000)
 julia> C = alloc!(buf, 10, 10, 20) # 10x10x20 destination tensor on a single thread
-julia> tbuf = ThreadsBuffer(100000) # 100000 elements buffer for nthreads() threads
+julia> tbuf = ThreadsBuffer(1000) # 1000 elements buffer for nthreads() threads each
 julia> Threads.@threads for k = 1:20
           A = alloc!(tbuf, 10, 10) # 10x10 tensor
           B = alloc!(tbuf, 10, 10) # 10x10 tensor
@@ -214,14 +217,40 @@ julia> Threads.@threads for k = 1:20
 """ 
 struct ThreadsBuffer{T}
     buffers::Vector{Buffer{T}}
+    pool::Vector{Int}
     condition::Threads.Condition
     id::Symbol
 end
 
-ThreadsBuffer{T}(len::Int, n::Int=Threads.nthreads()) where T = ThreadsBuffer{T}([Buffer{T}(len) for _ in 1:n], Threads.Condition(), gensym(:tbuffer))
+function ThreadsBuffer{T}(len::Int, n::Int=Threads.nthreads()) where T
+  ThreadsBuffer{T}([Buffer{T}(len) for _ in 1:n], [1:n;], Threads.Condition(), gensym(:tbuffer))
+end
 ThreadsBuffer(len::Int, n::Int=Threads.nthreads()) = ThreadsBuffer{Float64}(len, n)
 
+"""
+    nbuffers(buf::ThreadsBuffer)
+
+Return the number of buffers in `buf::ThreadsBuffer`.
+"""
 nbuffers(buf::ThreadsBuffer) = length(buf.buffers)
+
+"""
+    current_buffer_index(buf::ThreadsBuffer)
+
+Return the index of the buffer of the current thread.
+
+If the buffer is not available, wait until it is released.
+"""
+function current_buffer_index(buf::ThreadsBuffer)
+  get!(task_local_storage(), buf.id) do
+    lock(buf.condition) do
+      while isempty(buf.pool)
+        wait(buf.condition)
+      end
+      return pop!(buf.pool)
+    end
+  end
+end
 
 """
     current_buffer(buf::ThreadsBuffer{T})
@@ -231,15 +260,7 @@ Return the buffer of the current thread.
 If the buffer is not available, wait until it is released.
 """
 function current_buffer(buf::ThreadsBuffer{T}) where {T}
-  b::Buffer{T} = get!(task_local_storage(), buf.id) do 
-    lock(buf.condition) do
-      while isempty(buf.buffers)
-        wait(buf.condition)
-      end
-      pop!(buf.buffers)
-    end
-  end
-  return b
+  return buf.buffers[current_buffer_index(buf)]
 end
 
 Base.length(buf::ThreadsBuffer) = length(current_buffer(buf))
@@ -292,9 +313,24 @@ Release buffer of the current thread.
 function release!(buf::ThreadsBuffer)
   lock(buf.condition) do
     @assert used(current_buffer(buf)) == 0 "Buffer is not empty! Use reset! to release the buffer."
-    push!(buf.buffers, current_buffer(buf))
+    push!(buf.pool, current_buffer_index(buf))
     delete!(task_local_storage(), buf.id)
     notify(buf.condition)
+  end
+end
+
+"""
+    repair!(buf::ThreadsBuffer)
+
+Repair ThreadsBuffer `buf` by releasing all buffers and resetting the pool.
+
+This function should be used after the threaded loop 
+if the buffers were not released properly.
+"""
+function repair!(buf::ThreadsBuffer)
+  for i in 1:nbuffers(buf)
+    reset!(buf.buffers[i])
+    push!(buf.pool, i)
   end
 end
 
@@ -320,9 +356,9 @@ end
 
 # Example
 ```julia
-julia> buf = Buffer(100000)
+julia> buf = Buffer(10000)
 julia> C = alloc!(buf, 10, 10, 20) # 10x10x20 destination tensor on a single thread
-julia> tbuf = ThreadsBuffer(100000)
+julia> tbuf = ThreadsBuffer(1000)
 julia> Threads.@threads for k = 1:20
           with_buffer(tbuf) do bu
             A = alloc!(bu, 10, 10) # 10x10 tensor
