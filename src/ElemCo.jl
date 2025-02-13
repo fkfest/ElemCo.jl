ef955b8492e7a2632c7846e7c4a6175febb37ba4
Daniel Kats <d.kats@fkf.mpg.de>
2025-02-13 20:34:57 +0100

    Update the macro description

--- src/ElemCo.jl
+++ src/ElemCo.jl
@@ -713,6 +713,9 @@ end
   Freeze orbitals in the integrals according to an array or range 
   `freeze_orbs`.
 
+  Alternatively, the orbitals can be specified as a String with the +/- or :/; syntax, e.g.,
+  "1-5+7-8", or "1:5;7-8".
+
   # Examples
 ```julia
 fcidump = "FCIDUMP"
