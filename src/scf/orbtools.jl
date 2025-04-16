1900560b8448a2467598c579bc2bdd68fa464973
Kristoffer Simula <k.simula@fkf.mpg.de>
2025-04-16 09:16:22 +0200

    load_orbitals now also loads positron orbitals

--- src/scf/orbtools.jl
+++ src/scf/orbtools.jl
@@ -134,13 +134,25 @@ end
 
   Returns `::SpinMatrix`. 
 """
-function load_orbitals(EC::ECInfo, orbsfile::String="")
+function load_orbitals(EC::ECInfo, orbsfile::String="", porbsfile::String="")
   if !isempty(strip(orbsfile))
     # orbsfile will be used
   elseif !isempty(strip(EC.options.wf.orb))
     orbsfile = EC.options.wf.orb
   else
     error("no orbitals found")
+  end
+  if EC.options.wf.npositron > 0
+    if !isempty(strip(porbsfile))
+      # orbsfile will be used
+    elseif !isempty(strip(EC.options.wf.orb_pos))
+      porbsfile = EC.options.wf.orb_pos
+    else
+      error("no positron orbitals found")
+    end
+    cMO = SpinMatrix(load_all(EC, orbsfile, Val(2))...)
+    cPO = SpinMatrix(load_all(EC, porbsfile, Val(2))...)
+    return cMO, cPO
   end
   return SpinMatrix(load_all(EC, orbsfile, Val(2))...)
 end
