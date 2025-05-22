d2814c2d1c569a24fe9efaa2c84a0f94207b6768
Kristoffer Simula <k.simula@fkf.mpg.de>
2025-05-22 16:49:04 +0200

    MP2 tests included, also with dummys

--- test/pos_df_hf.jl
+++ test/pos_df_hf.jl
@@ -4,26 +4,30 @@ using ElemCo
 epsilon    =  1.e-6
 EHF_H_test     =     -0.660770127162853
 Ecorr_MP2_H_test     =     -0.03503188483173242
-EHF_LiH_test   =      -7.991998796257979
+EHF_LiH_test   =      -7.988745934771541
+Ecorr_MP2_LiH_test   =      -0.034701361656416636
 
 xyz_H="bohr
 H 0.000000 0.000000 0.000000"
 
 xyz_LiH="bohr
-            Li 0.000000 0.000000 0.000000
-            H  0.000000 0.000000 3.0196"
+            Li 0.000000  0.000000 0.000000
+            H  0.000000  0.000000 3.0196
+            H2 0.000000 -1.000000 4.0196
+            H2 0.000000  1.000000 4.0196"
 
 
 basis_H = Dict("ao"=>"aug-cc-pVDZ",
      "jkfit"=>"def2-universal-jkfit",
      "mpfit"=>"cc-pvtz-rifit")
-basis_LiH = Dict("ao"=>"aug-cc-pVqZ",
+basis_LiH = Dict("ao"=>"aug-cc-pVdZ",
      "jkfit"=>"def2-universal-jkfit",
-     "mpfit"=>"cc-pvqz-rifit")
+     "mpfit"=>"cc-pvtz-rifit")
 
 EC = ElemCo.ECInfo(system=ElemCo.parse_geometry(xyz_H,basis_H))
 @set wf charge=-1
 @set wf npositron=1
+@set wf freeze_nocc=0
 E_H=@dfhf
 @dfints
 E_H = @cc MP2
@@ -33,11 +37,18 @@ E_H = @cc MP2
 EC = ElemCo.ECInfo(system=ElemCo.parse_geometry(xyz_LiH,basis_LiH))
 @set wf charge=0
 @set wf npositron=1
+@set wf freeze_nocc=0
+@dummy["H2"]
 E_LiH=@dfhf
+@dfints
+E_LiH = @cc MP2
 @test abs(E_LiH["HF"]-EHF_LiH_test) < epsilon
+@test abs(E_LiH["MP2c"]-Ecorr_MP2_LiH_test) < epsilon
 EC = ElemCo.ECInfo(system=ElemCo.parse_geometry(xyz_LiH,basis_LiH))
 @set wf charge=0
 @set wf npositron=1
+@set wf freeze_nocc=0
+@dummy["H2"]
 @set scf direct=1
 E_LiH_direct=@dfhf
 @test abs(E_LiH_direct["HF"]-EHF_LiH_test) < epsilon
