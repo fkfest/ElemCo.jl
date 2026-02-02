bba5f1e3a283bd180747c753477c97861ee948b2
Kristoffer Simula <k.simula@fkf.mpg.de>
2026-02-02 19:03:00 +0100

    Refactor of positron tests, and update of MP2 correlation energies after a bug fix.

--- test/pos_df_hf.jl
+++ test/pos_df_hf.jl
@@ -1,56 +1,84 @@
 using ElemCo
 
-@testset "Positron DF-HF Closed-Shell Test" begin
-epsilon    =  1.e-6
-EHF_H_test     =     -0.660770127162853
-Ecorr_MP2_H_test     =     -0.03503188483173242
-EHF_LiH_test   =      -7.988745934771541
-Ecorr_MP2_LiH_test   =      -0.034701361656416636
+@testset "Positron DF-HF Test" begin
 
-xyz_H="bohr
-H 0.000000 0.000000 0.000000"
+  epsilon = 1.e-6
 
-xyz_LiH="bohr
-            Li 0.000000  0.000000 0.000000
-            H  0.000000  0.000000 3.0196
-            H2 0.000000 -1.000000 4.0196
-            H2 0.000000  1.000000 4.0196"
+  # Reference energies
+  EHF_H_ref        = -0.660770127162853
+  Ecorr_MP2_H_ref  = 0.008491457275
+  EHF_LiH_ref      = -7.988745934771541
+  Ecorr_MP2_LiH_ref = -0.343860916981
 
+  # Geometries
+  xyz_H = """
+    bohr
+    H 0.0 0.0 0.0
+  """
 
-basis_H = Dict("ao"=>"aug-cc-pVDZ",
-     "jkfit"=>"def2-universal-jkfit",
-     "mpfit"=>"cc-pvtz-rifit")
-basis_LiH = Dict("ao"=>"aug-cc-pVdZ",
-     "jkfit"=>"def2-universal-jkfit",
-     "mpfit"=>"cc-pvtz-rifit")
+  xyz_LiH = """
+    bohr
+    Li 0.0  0.0  0.0
+    H  0.0  0.0  3.0196
+    H2 0.0 -1.0  4.0196
+    H2 0.0  1.0  4.0196
+  """
 
-EC = ElemCo.ECInfo(system=ElemCo.parse_geometry(xyz_H,basis_H))
-@set wf charge=-1
-@set wf npositron=1
-@set wf freeze_nocc=0
-E_H=@dfhf
-@dfints
-E_H = @cc MP2
-@show E_H
-@test abs(E_H["HF"]-EHF_H_test) < epsilon
-@test abs(E_H["MP2c"]-Ecorr_MP2_H_test) < epsilon
-EC = ElemCo.ECInfo(system=ElemCo.parse_geometry(xyz_LiH,basis_LiH))
-@set wf charge=0
-@set wf npositron=1
-@set wf freeze_nocc=0
-@dummy["H2"]
-E_LiH=@dfhf
-@dfints
-E_LiH = @cc MP2
-@test abs(E_LiH["HF"]-EHF_LiH_test) < epsilon
-@test abs(E_LiH["MP2c"]-Ecorr_MP2_LiH_test) < epsilon
-EC = ElemCo.ECInfo(system=ElemCo.parse_geometry(xyz_LiH,basis_LiH))
-@set wf charge=0
-@set wf npositron=1
-@set wf freeze_nocc=0
-@dummy["H2"]
-@set scf direct=1
-E_LiH_direct=@dfhf
-@test abs(E_LiH_direct["HF"]-EHF_LiH_test) < epsilon
+  # Basis sets
+  basis_H = Dict(
+    "ao"    => "aug-cc-pVDZ",
+    "jkfit" => "def2-universal-jkfit",
+    "mpfit" => "cc-pvtz-rifit"
+  )
+
+  basis_LiH = Dict(
+    "ao"    => "aug-cc-pVDZ",
+    "jkfit" => "def2-universal-jkfit",
+    "mpfit" => "cc-pvtz-rifit"
+  )
+
+  #---------------------------------------------------------------------------
+  @testset "H⁻ + positron" begin
+    EC = ElemCo.ECInfo(system=ElemCo.parse_geometry(xyz_H, basis_H))
+    @set wf charge = -1
+    @set wf npositron = 1
+    @set wf freeze_nocc = 0
+
+    E_H = @dfhf
+    @test abs(E_H["HF"] - EHF_H_ref) < epsilon
+
+    @dfints
+    E_H = @cc MP2
+    @test abs(E_H["MP2c"] - Ecorr_MP2_H_ref) < epsilon
+  end
+
+  #---------------------------------------------------------------------------
+  @testset "LiH + positron" begin
+    EC = ElemCo.ECInfo(system=ElemCo.parse_geometry(xyz_LiH, basis_LiH))
+    @set wf charge = 0
+    @set wf npositron = 1
+    @set wf freeze_nocc = 0
+    @dummy["H2"]
+
+    E_LiH = @dfhf
+    @test abs(E_LiH["HF"] - EHF_LiH_ref) < epsilon
+
+    @dfints
+    E_LiH = @cc MP2
+    @test abs(E_LiH["MP2c"] - Ecorr_MP2_LiH_ref) < epsilon
+  end
+
+  #---------------------------------------------------------------------------
+  @testset "LiH + positron (direct SCF)" begin
+    EC = ElemCo.ECInfo(system=ElemCo.parse_geometry(xyz_LiH, basis_LiH))
+    @set wf charge = 0
+    @set wf npositron = 1
+    @set wf freeze_nocc = 0
+    @set scf direct = 1
+    @dummy["H2"]
+
+    E_LiH_direct = @dfhf
+    @test abs(E_LiH_direct["HF"] - EHF_LiH_ref) < epsilon
+  end
 
 end
