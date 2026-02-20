d1923f1969f7587cb61be690a19c7c45a96e0d4b
Daniel Kats <d.kats@fkf.mpg.de>
2026-02-20 16:10:39 +0100

    Use one-iter-SVD-DCD to obtain the SVD space for SVD-DCSD

--- src/cc/dfcc.jl
+++ src/cc/dfcc.jl
@@ -412,7 +412,7 @@ function calc_doubles_decomposition_without_doubles(EC::ECInfo)
   end
   t1 = print_time(EC, t1, "MP2 from 3idx", 2)
   flush_output()
-  if EC.options.cc.use_full_t2
+  if EC.options.cc.use_full_t2 || !EC.options.cc.decompose_using_mp2
     T2 = try2start_doubles(EC)
     if size(T2) != (nvirt,nvirt,nocc,nocc)
       T2 = calc_MP2_amplitudes_from_3idx(EC, voL, shifti)
@@ -447,17 +447,21 @@ function calc_doubles_decomposition_without_doubles(EC::ECInfo)
   t1 = print_time(EC, t1, "SVD decomposition", 2)
   # UaiX = calc_3idx_svd_decomposition(EC, voL, tol2) 
   ϵX, UaiX = rotate_U2pseudocanonical(EC, UaiX)
-  # calculate rhs: v_{aX}^{i} = v_a^{iL} 𝟙_{LL} v_b^{jL} (U_b^{jX})^† 
-  @mtensor voX[a,i,X] := voL[a,i,L] * (voL[b,j,L] * UaiX[b,j,X])
-  # calculate half-decomposed imaginary-shifted MP2 amplitudes 
-  # T^i_{aX} = -v_{aX}^{i} * (ϵ_a - ϵ_i + ϵ_X)/((ϵ_a - ϵ_i - ϵ_X)^2 + ω)
-  # TODO: use a better method than MP2
-  for I ∈ CartesianIndices(voX)
-    a,i,X = Tuple(I)
-    den = ϵX[X] + ϵv[a] - ϵo[i]
-    voX[I] *= -den/(den^2 + shifti)
+  if EC.options.cc.decompose_using_mp2
+    # calculate rhs: v_{aX}^{i} = v_a^{iL} 𝟙_{LL} v_b^{jL} (U_b^{jX})^† 
+    @mtensor voX[a,i,X] := voL[a,i,L] * (voL[b,j,L] * UaiX[b,j,X])
+    # calculate half-decomposed imaginary-shifted MP2 amplitudes 
+    # T^i_{aX} = -v_{aX}^{i} * (ϵ_a - ϵ_i + ϵ_X)/((ϵ_a - ϵ_i - ϵ_X)^2 + ω)
+    for I ∈ CartesianIndices(voX)
+      a,i,X = Tuple(I)
+      den = ϵX[X] + ϵv[a] - ϵo[i]
+      voX[I] *= -den/(den^2 + shifti)
+    end
+    t1 = print_time(EC, t1, "half-decomposed MP2", 2)
+  else
+    T2 = calc_SVD_DCD_doubles1(EC, UaiX, ϵX)
+    @mtensor voX[a,i,X] := T2[a,b,i,j] * UaiX[b,j,X]
   end
-  t1 = print_time(EC, t1, "half-decomposed MP2", 2)
   naux = size(voX, 3)
   # decompose T^i_{aX}
   UaiX = svd_decompose(reshape(voX, (nvirt*nocc,naux)), nvirt, nocc, sqrt(EC.options.cc.ampsvdtol))
@@ -562,6 +566,35 @@ function calc_3idx_svd_decomposition(EC::ECInfo, voL::AbstractArray, tol2)
   return UaiX
 end
 
+"""
+    calc_SVD_DCD_doubles1(EC::ECInfo, UaiX, ϵX)
+
+  Calculate one iteration of DCD. 
+
+Required for the second decomposition step. A single SVD-DCD iteration is performed with 
+the decomposed doubles amplitudes (using MP2 SVD space).
+
+Returns the updated doubles amplitudes ``T^{ij}_{ab}``.
+"""
+function calc_SVD_DCD_doubles1(EC::ECInfo, UaiX, ϵX)
+  println("Calculate SVD DCD doubles decomposition")
+  project_vovo_t2_save = EC.options.cc.project_vovo_t2
+  EC.options.cc.project_vovo_t2 = 1
+  use_projx_save = EC.options.cc.use_projx
+  EC.options.cc.use_projx = false
+  save!(EC, "C_voX", UaiX)
+  save!(EC, "e_X", ϵX)
+  T1 = zeros(0,0)
+  save_pseudodressed_3idx(EC)
+  save_pseudo_dress_df_fock(EC)
+  gen_vₓˣᴸ(EC)
+  T2 = load4idx(EC, "T_vvoo")
+  R1, R2 = calc_svd_dcsd_residual(EC, T1, T2)
+  T2 .+= update_deco_doubles(EC, R2)
+  EC.options.cc.project_vovo_t2 = project_vovo_t2_save
+  EC.options.cc.use_projx = use_projx_save 
+  return T2
+end
 
 """
     calc_MP2_from_3idx(EC::ECInfo, voL::AbstractArray, ishift)
